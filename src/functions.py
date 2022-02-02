e03cc9515cb4bbb0ff94fafe7d62bc9477976dd2
evaarquero <eva.arquero@gmail.com>
2022-02-02 12:13:20 +0100

    data-cleaning-pandas

--- src/functions.py
+++ src/functions.py
@@ -8,15 +8,5 @@ import regex as re
 import pandas as pd
 import re
 
-def month(string):
-    """This digit looks for any 4 digits it can find in the string and returns them as an integer, if it doesn't find 
-    any number it returns a 0."""
-    try:
-        a = re.findall("\d{4}", str(string))
-        if a != None:
-            return a[0]
-        else:
-            return 0
-    except IndexError:
-        return 0
+
 
